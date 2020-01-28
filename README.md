edbd8924a4e3a184a7f1dea764721929b0fa596d
Yaniv <yanivou.bbb@gmail.com>
2020-01-28 14:28:42 +0100

    méthodes et Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Ceci est un programme Ruby.
+C'est un jeu ou 2 joueurs s'affrontent sur une base de 10 points de vie.
+Chaque joueur attaque au tour par tour et inflige entre 1 et 6 points de dégats a l'autre joueur.
+Le jeu se poursuit jusqu'a ce que l'un des deux joueurs meurt.
--- lib/player.rb
+++ lib/player.rb
@@ -1,20 +1,20 @@
 require 'pry'
 
-class Player
+class Player #initialisation de la class
 attr_accessor :name ,  :life_points #2 attributs
 
-def initialize (name )
+def initialize (name ) #méthode initialize a une entrée
 
 @name = name
 @life_points = 10
 
 end #fin def initialize
 
-def show_state
+def show_state #méthode qui montre l'état du joueur
 puts "#{@name} a #{@life_points} points de vie"
 end #fin de show_state
 
-def gets_damage(damage)
+def gets_damage(damage)#méthode pour infliger des dégats
  @life_points = @life_points - damage
 
 	if @life_points <= 0 
@@ -23,7 +23,7 @@ def gets_damage(damage)
 
 end #fin de gets_damage
 
-def attacks(player)
+def attacks(player) #méthode d'attaque des joueurs
 
 puts "Le joueur #{name} attaque le joueur #{player.name}"
 random= compute_damage
@@ -31,7 +31,7 @@ puts " Il lui inflige #{random} points de dommages"
 player.gets_damage(random)
 end #fin de attacks
 
-def compute_damage
+def compute_damage #génère un dégat aléatoire entre 1 et 6
 
 return rand(1..6)
 
